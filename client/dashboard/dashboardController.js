8a5310c094a1d4ea541d877ac1e08ed4dc01888b
kazlan <jorge.mollon@gmail.com>
2015-10-11 20:30:03 +0200

    merge meteorizedb branch

--- client/dashboard/dashboardController.js
+++
@@ -1,9 +0,0 @@
-angular
-	.module('optikApp')
-	.controller('dashboardController', dashboardController);
-
-dashboardController.$inject= ['$scope'];
-
-function dashboardController($scope){
-	$scope.initMsg ="Eips";
-}
--- client/mainController.js
+++
@@ -1,9 +0,0 @@
-angular
-	.module('optikApp')
-	.controller('mainController', mainController);
-	
-	mainController.$inject= ['$scope'];
-	
-	function mainController($scope){
-
-	};
